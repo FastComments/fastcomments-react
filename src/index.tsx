77aaab6d8f9cfbb16493202a1bd36e2fe270e495
winrid <winrid@gmail.com>
2020-08-10 22:18:52 -0700

    functional version

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,89 @@
 import * as React from 'react'
-//import { isEqual } from 'lodash';
-import styles from './styles.module.css'
+import {isEqual} from 'lodash';
 
-interface Props {
-  text: string
+interface FastCommentsConfig {
+  text: string,
+  widgetId?: string,
 }
 
-export class ExampleComponent extends React.Component<Props, {}> {
+enum LoadStatus {
+  Started,
+  Done,
+  Error
+};
 
-  /*
-  componentDidMount() {
-    this.loadInstance();
+interface FastCommentsState {
+  status: LoadStatus,
+  widgetId: string
+}
+
+export class ExampleComponent extends React.Component<FastCommentsConfig, FastCommentsState> {
+
+  constructor(props: FastCommentsConfig) {
+    super(props);
+    this.state = {
+      status: LoadStatus.Started,
+      widgetId: props.widgetId || 'fastcomments-widget-' + new Date()
+    };
   }
 
-  shouldComponentUpdate(nextState: Props) {
-    if (this.state === nextState)
-      return false;
+  componentDidMount() {
+    return this.loadInstance();
+  }
+
+  shouldComponentUpdate(nextState: FastCommentsConfig) {
     return isEqual(this.state, nextState);
   }
 
   componentDidUpdate() {
-    this.cleanInstance();
-    this.loadInstance();
+    return this.loadInstance();
   }
 
-  loadInstance() {
-    insertScript('https://cdn.fastcomments.com/js/embed.min.js', 'test', window.document.body);
+  async insertScript(src: string, id: string, parentElement: Element) {
+    return new Promise((resolve, reject) => {
+      const script = window.document.createElement('script');
+      script.async = true;
+      script.src = src;
+      script.id = id;
+      parentElement.appendChild(script);
+
+      script.addEventListener('load', resolve);
+      script.addEventListener('error', reject);
+    });
   }
 
-  cleanInstance() {
-    // TODO
+  async loadInstance() {
+    switch (this.state.status) {
+      case LoadStatus.Started:
+        try {
+          // @ts-ignore
+          if (window && !window.FastCommentsUI) {
+            await this.insertScript('https://cdn.fastcomments.com/js/embed.min.js', 'fastcomments-widget-script', window.document.body);
+          }
+          this.setState({
+            status: LoadStatus.Done
+          });
+          this.forceUpdate();
+        } catch (e) {
+          console.error('FastComments Script Load Failure', e);
+          this.setState({
+            status: LoadStatus.Error
+          });
+          this.forceUpdate();
+        }
+        break;
+      case LoadStatus.Done:
+        // @ts-ignore
+        window.FastCommentsUI(document.getElementById(this.state.widgetId), {
+          tenantId: 'demo'
+        });
+        break;
+    }
   }
-  */
 
   render() {
-    return <div className={styles.test}>Example test Component: {this.props.text}</div>
+    return (
+      <div id={this.state.widgetId}>{this.state.status === LoadStatus.Error ? 'Oh no! The comments section could not be loaded.' : ''}</div>
+    )
   }
 }
-
-/*
-function insertScript(src : string, id : string, parentElement : Element) {
-  const script = window.document.createElement('script');
-  script.async = true;
-  script.src = src;
-  script.id = id;
-  parentElement.appendChild(script);
-
-  return script;
-}
-*/
