927b6d106ed07d8ae8874db250f8a84fdde944d0
winrid <winrid@gmail.com>
2021-09-08 19:47:30 -0700

    Exporting required configuration structures

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-export {FastCommentsCommentWidgetConfig, FastCommentsCommentCountConfig, FastCommentsSSO} from 'fastcomments-typescript';
+export {FastCommentsCommentWidgetConfig, FastCommentsCommentCountConfig, FastCommentsSSO, FastCommentsLiveChatWidgetConfig} from 'fastcomments-typescript';
 export {FastCommentsCommentCountWidget} from './comment-count-widget';
 export {FastCommentsCommentWidget} from './comment-widget';
-export {FastCommentsCollabChatWidget} from './collab-chat-widget';
-export {FastCommentsImageChatWidget} from './image-chat-widget';
+export {FastCommentsCollabChatWidget, FastCommentsCollabChatWidgetConfigReact} from './collab-chat-widget';
+export {FastCommentsImageChatWidget, FastCommentsImageChatWidgetConfigReact} from './image-chat-widget';
 export {FastCommentsLiveChatWidget} from './live-chat-widget';
