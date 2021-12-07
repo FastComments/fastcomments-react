40eea5c48110ee40803dfe35b2a08f91f94415b7
winrid <winrid@gmail.com>
2021-12-06 17:59:51 -0800

    updating comment count example

--- examples/example-comment-count/src/CommentCountApp.tsx
+++ examples/example-comment-count/src/CommentCountApp.tsx
@@ -3,7 +3,7 @@ import {FastCommentsCommentCountWidget} from "fastcomments-react";
 
 const CommentCountApp = () => {
   return (
-    <FastCommentsCommentCountWidget tenantId="demo" />
+    <FastCommentsCommentCountWidget tenantId="demo" urlId="https://example.com/some-page-or-id" />
   )
 }
 
