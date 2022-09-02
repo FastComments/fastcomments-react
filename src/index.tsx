c1efcfd0e12f12268559d7f1b4c53bd20aeb2374
winrid <winrid@gmail.com>
2022-09-01 17:44:44 -0700

    switching to export *

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-export {FastCommentsCommentWidgetConfig, FastCommentsCommentCountConfig, FastCommentsSSO, FastCommentsLiveChatWidgetConfig} from 'fastcomments-typescript';
-export {FastCommentsCommentCountWidget} from './comment-count-widget';
-export {FastCommentsCommentWidget} from './comment-widget';
-export {FastCommentsCollabChatWidget, FastCommentsCollabChatWidgetConfigReact} from './collab-chat-widget';
-export {FastCommentsImageChatWidget, FastCommentsImageChatWidgetConfigReact} from './image-chat-widget';
-export {FastCommentsLiveChatWidget} from './live-chat-widget';
+export * from 'fastcomments-typescript';
+export * from './comment-count-widget';
+export * from './comment-widget';
+export * from './collab-chat-widget';
+export * from './image-chat-widget';
+export * from './live-chat-widget';
+export * from './user-activity-feed-widget';
