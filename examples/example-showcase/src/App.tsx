1bc6599d15ae5835cadbb5c9ba9d913955b1f22f
winrid <winrid@gmail.com>
2026-05-31 15:09:48 -0700

    Sidebar: drop arrow glyph from fastcomments.com link

--- examples/example-showcase/src/App.tsx
+++ examples/example-showcase/src/App.tsx
@@ -142,7 +142,7 @@ function App() {
             <button type="button" className={`fc-theme-toggle__btn${theme === 'dark' ? ' is-active' : ''}`} onClick={() => setTheme('dark')}>Dark</button>
           </div>
           <div><code>npm i fastcomments-react</code></div>
-          <a href="https://fastcomments.com" rel="noopener">fastcomments.com ↗</a>
+          <a href="https://fastcomments.com" rel="noopener">fastcomments.com</a>
         </footer>
       </aside>
 
